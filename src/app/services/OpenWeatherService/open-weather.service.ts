8b53c9628af3888c69378dfe8f9b115cc2b505f4
Manuel Meneses <maeneser@icloud.com>
2020-02-13 16:50:33 -0600

    Open weather service

---
+++ src/app/services/OpenWeatherService/open-weather.service.ts
@@ -0,0 +1,23 @@
+import { environment } from '../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Models
+import { OpenWeather } from '../../models/OpenWeather/open-weather';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenWeatherService {
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherById(id: number): Observable < OpenWeather > {
+    return this.http.get < OpenWeather > (environment.openWeatherApi + '?id=' + id + "&units=metric" + '&appid=' + environment.openWeatherApiKey);
+  }
+
+  getWeatherByCoordinates(lat: number, lon: number): Observable < OpenWeather > {
+    return this.http.get < OpenWeather > (environment.openWeatherApi + '?lat=' + lat + '&lon=' + lon + "&units=metric" + '&appid=' + environment.openWeatherApiKey);
+  }
+}
