f38d718cd3ee31f0196efbf8e13b06bdbaab18ff
Manuel Meneses <maeneser@icloud.com>
2020-02-13 18:04:06 -0600

    Search component

--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Models
+import { OpenWeather } from 'src/app/models/OpenWeather/open-weather';
+
+// Services
+import { OpenWeatherService } from '../../services/OpenWeatherService/open-weather.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private openWeatherService: OpenWeatherService) {}
+
+  ngOnInit() {
+    this.openWeatherService.getWeatherById(707860).subscribe(
+      (data: OpenWeather) => console.log(data),
+      (err) => console.log(err)
+    );
+  }
 
 }
