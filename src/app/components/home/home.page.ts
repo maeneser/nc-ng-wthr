e1434f2c8c4cd304b31316ffcb13efb6158d7245
Manuel Meneses <maeneser@icloud.com>
2020-02-17 17:04:20 -0600

    Delete cities

--- src/app/components/home/home.page.ts
+++ src/app/components/home/home.page.ts
@@ -7,13 +7,13 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private cities = new Array<string>();
+  private cities = new Array<number>();
 
   constructor(public toastController: ToastController) {}
 
   ngOnInit() {}
 
-  async addCity(city: string) {
+  async addCity(city: number) {
     let i = this.cities.indexOf(city);
     if (i == -1) this.cities.push(city);
     else {
@@ -23,5 +23,11 @@ export class HomePage implements OnInit {
       });
       toast.present();
     }
+  }
+
+  deleteCity(city: number) {
+    let i = this.cities.indexOf(city);
+    if(i != -1)
+      this.cities.splice(i, 1);
   }
 }
--- src/app/components/home/weather/weather.component.ts
+++ src/app/components/home/weather/weather.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 
 // Models
@@ -14,6 +14,7 @@ import { OpenWeatherService } from '../../../services/OpenWeatherService/open-we
 })
 export class WeatherComponent implements OnInit {
   @Input() city: number;
+  @Output() deleteCity = new EventEmitter<number>();
   private openWeather: OpenWeather;
   private loaded = false;
 
@@ -33,6 +34,10 @@ export class WeatherComponent implements OnInit {
         toast.present();
       }
     );
+  }
+
+  delete() {
+    this.deleteCity.emit(this.city);
   }
 
 }
